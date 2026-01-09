d8dc3cfcf7c0095fde020bbfa10e775dfa4f7a4a
Inguim <igorazevedost@gmail.com>
2026-01-08 21:13:24 -0300

    feat: adiciona migration de 'usuarios'

---
+++ src/database/migrations/20260104174432_users.ts
@@ -0,0 +1,29 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable("usuarios", (table) => {
+		table.uuid("id").primary();
+		table.string("nome", 255).notNullable();
+		table.string("email", 255).notNullable().unique();
+		table
+			.enu("tipo", ["aluno", "professor"], {
+				useNative: true,
+				enumName: "user_tipo",
+			})
+			.notNullable();
+		table.boolean("admin").notNullable().defaultTo(false);
+		table
+			.enu("status", ["aprovado", "analise"], {
+				useNative: true,
+				enumName: "user_status",
+			})
+			.notNullable()
+			.defaultTo("analise");
+		table.string("password", 255).notNullable();
+		table.timestamps(true, true);
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTableIfExists("users");
+}
