bbbd5ba991c79fce847c4cf9837d5257f380e135
Inguim <igorazevedost@gmail.com>
2026-01-11 12:16:05 -0300

    refactor: refatora a logica de comunicao entre model e services
    As logicas de troca de informação entre model e service estavam misturadas, o que estava acarretando
    em bugs de persistência ja que informações estavam vazando entre as camadas

--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -1,24 +1,17 @@
-import type { IUsuarioDTO } from "../dto/UsuarioDTO.js";
+import { UsuarioDTO, type IUsuarioDTO } from "../dto/UsuarioDTO.js";
 import { PasswordService } from "../services/PasswordService.js";
 import { BaseModel } from "./BaseModel.js";
 
 export class Usuario extends BaseModel<IUsuarioDTO> {
-	nome = "";
-	email = "";
-	password = "";
-	tipo = "aluno";
-	status = "analise";
-	admin = false;
-
 	private passwordService = new PasswordService();
 
 	constructor(fields: Partial<IUsuarioDTO> = {}) {
-		super(fields);
+		super(new UsuarioDTO(fields));
 		this.table = "usuarios";
 		this.tableTag = "Usuário";
 	}
 
 	protected async beforeCreate(): Promise<void> {
-		this.password = await this.passwordService.hash(this.password);
+		this.dto.password = await this.passwordService.hash(this.dto.password);
 	}
 }
