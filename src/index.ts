345321effe3a729a0fdd94909a9abdfcf39e3af4
Inguim <igorazevedost@gmail.com>
2026-01-11 20:30:39 -0300

    build: adiciona documentação com Swagger

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,35 @@ import "dotenv/config";
 import express from "express";
 import { routes } from "./routes/index.js";
 import { errorHandlingMiddleware } from "./middlewares/index.js";
-interface AppConfig {
-	port: number;
-}
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url";
+import SwaggerParser from "@apidevtools/swagger-parser";
+import { getEnv } from "./utils/helpers/getEnv.js";
 
-const config: AppConfig = {
-	port: process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 3000,
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const openApiPath = path.resolve(__dirname, "../docs/openapi.yaml");
 
 const app = express();
+
+const APP_PORT = getEnv("APP_PORT");
 
 routes(app);
 
 app.use(errorHandlingMiddleware);
 
+(async () => {
+	const api = await SwaggerParser.bundle(openApiPath);
+
+	app.use("/docs", swaggerUi.serve, swaggerUi.setup(api));
+})();
+
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 
-app.listen(config.port, () => {
-	console.log(`Server is running on port ${config.port}`);
+app.listen(APP_PORT, () => {
+	console.log(`Server is running on port ${APP_PORT}`);
 });
