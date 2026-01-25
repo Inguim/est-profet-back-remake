4ea3958cc020116c2ff0ef6f4f94742d5c7e7d2b
Inguim <igorazevedost@gmail.com>
2026-01-25 16:29:13 -0300

    feat: adiciona comparação de senhas so PasswordService

--- src/services/PasswordService.test.ts
+++ src/services/PasswordService.test.ts
@@ -2,11 +2,24 @@ import { describe, expect, it } from "vitest";
 import { PasswordService } from "./PasswordService.js";
 import { faker as f } from "@faker-js/faker";
 
+const passwordService = new PasswordService();
+
 describe("PasswordService", () => {
 	it("deve gerar o hash de uma senha", async () => {
-		const passwordService = new PasswordService();
 		const input = f.lorem.word({ length: { min: 8, max: 16 } });
 		const output = await passwordService.hash(input);
 		expect(output).not.toEqual(input);
+	});
+
+	it("deve retornar true para senhas iguais", async () => {
+		const input = await passwordService.hash("senha");
+		const output = await passwordService.compare("senha", input);
+		expect(output).toBeTruthy();
+	});
+
+	it("deve retornar false para senhas diferentes", async () => {
+		const input = await passwordService.hash("senha");
+		const output = await passwordService.compare("senhA", input);
+		expect(output).toBeFalsy();
 	});
 });
