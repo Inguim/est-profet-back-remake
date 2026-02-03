9ff7059e027d89b9891d07f15f71e791b895e2ce
Inguim <igorazevedost@gmail.com>
2026-02-03 18:00:32 -0300

    feat: adiciona interface IUsuarioService para melhor extensão do código

--- src/services/UsuarioService.ts
+++ src/services/UsuarioService.ts
@@ -1,12 +1,23 @@
 import { UsuarioDTO } from "../dto/index.js";
 import { UsuarioModel } from "../models/index.js";
-import { BaseService } from "./BaseService.js";
+import { BaseService, type IBaseService } from "./BaseService.js";
 
 type TCreateDTO = Pick<UsuarioDTO, "nome" | "email" | "tipo" | "password">;
-type TUpdateDTO = Pick<UsuarioDTO, "nome" | "email" | "tipo">;
+type TUpdateDTO = Partial<Pick<UsuarioDTO, "nome" | "email" | "tipo">>;
 export type TFindOneDTO = Partial<Pick<UsuarioDTO, "email">>;
 
-export class UsuarioService extends BaseService<UsuarioDTO, UsuarioModel, TCreateDTO, TUpdateDTO, TFindOneDTO> {
+export interface IUsuarioService extends IBaseService {
+	create(fields: TCreateDTO): Promise<UsuarioDTO>;
+	update(id: string, fields: TUpdateDTO): Promise<UsuarioDTO>;
+	delete(id: string): Promise<boolean>;
+	get(id: string): Promise<UsuarioDTO | null>;
+	findOne(where: TFindOneDTO): Promise<UsuarioDTO>;
+}
+
+export class UsuarioService
+	extends BaseService<UsuarioDTO, UsuarioModel, TCreateDTO, TUpdateDTO, TFindOneDTO>
+	implements IUsuarioService
+{
 	protected model = UsuarioModel;
 	protected dto = UsuarioDTO;
 }
