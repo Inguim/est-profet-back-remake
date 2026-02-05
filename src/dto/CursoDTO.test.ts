aa1ac7e1efb0a9665e93753931a3d0f8b580e565
Inguim <igorazevedost@gmail.com>
2026-02-05 15:43:47 -0300

    refactor: refatora declaração de construtor de CursoDTO
    Altera declaração de contrutor de CursoDTO com o intuito de esclarecer melhor o contrato do que deve
    ser informado ao instanciar o DTO

--- src/dto/CursoDTO.test.ts
+++ src/dto/CursoDTO.test.ts
@@ -1,15 +1,30 @@
 import { describe, expect, it } from "vitest";
-import { CursoDTO } from "./index.js";
+import { CursoDTO, type ICursoDTO } from "./index.js";
 
 describe("CursoDTO", () => {
 	it("deve conter as propriedades de ICursoDTO", () => {
-		const output = new CursoDTO();
+		const output = new CursoDTO({ curso: "teste" });
+		expect(output).toHaveProperty("id");
 		expect(output).toHaveProperty("curso");
+		expect(output).toHaveProperty("created_at");
+		expect(output).toHaveProperty("updated_at");
 	});
 
 	it("deve preencher automaticamente os campos não parciais", () => {
-		const output = new CursoDTO();
-		expect(output.created_at).toBeInstanceOf(Date);
-		expect(output.updated_at).toBeInstanceOf(Date);
+		const output = new CursoDTO({ curso: "teste" });
+		expect(output.id).toBeNull();
+		expect(output.created_at).toBeUndefined();
+		expect(output.updated_at).toBeUndefined();
+	});
+
+	it("deve preencher corretamente os campos", () => {
+		const input: ICursoDTO = {
+			id: "id123",
+			curso: "teste",
+			created_at: new Date(),
+			updated_at: new Date(),
+		};
+		const output = new CursoDTO(input);
+		expect(output).toEqual(input);
 	});
 });
--- src/dto/CursoDTO.ts
+++ src/dto/CursoDTO.ts
@@ -4,15 +4,16 @@ export interface ICursoDTO extends IBaseDTO {
 	curso: string;
 }
 
-type TFields = Partial<ICursoDTO>;
-
 export class CursoDTO implements ICursoDTO {
 	id?: string | null;
 	created_at?: Date;
 	updated_at?: Date;
-	curso: string = "";
+	curso: string;
 
-	constructor(fields: TFields = {}) {
-		Object.assign(this, fields);
+	constructor({ id = null, created_at, updated_at, curso }: ICursoDTO) {
+		this.id = id;
+		this.curso = curso;
+		if (created_at) this.created_at = created_at;
+		if (updated_at) this.updated_at = updated_at;
 	}
 }
