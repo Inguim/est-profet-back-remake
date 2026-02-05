7e212e430055625c9bfa4e28cd82169c31258eb6
Inguim <igorazevedost@gmail.com>
2026-02-05 19:45:26 -0300

    feat: adiciona controller SerieController

---
+++ src/controllers/SerieController.ts
@@ -0,0 +1,30 @@
+import type { Request, Response, NextFunction } from "express";
+import type { ISerieService } from "../services/index.js";
+import { STATUS_CODE } from "../utils/constansts/status-code.js";
+
+type TControllerConstructor = {
+	serieService: ISerieService;
+};
+
+export class SerieController {
+	private serieService: ISerieService;
+
+	constructor({ serieService }: TControllerConstructor) {
+		this.serieService = serieService;
+	}
+
+	async list(req: Request, res: Response, next: NextFunction): Promise<void> {
+		try {
+			const cursos = await this.serieService.list();
+			res.status(STATUS_CODE.CREATED).json({
+				message: "Series buscadas com sucesso",
+				data: {
+					count: cursos.length,
+					results: cursos,
+				},
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
+}
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from "./UsuarioController.js";
 export * from "./AuthController.js";
 export * from "./CursoController.js";
+export * from "./SerieController.js";
