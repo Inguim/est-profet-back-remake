6cbddd2753dbc0727c13203fb28dcdf38df72f51
Inguim <igorazevedost@gmail.com>
2026-02-05 14:43:05 -0300

    feat: adiciona migration de series

--- src/utils/constansts/default-constraints.ts
+++ src/utils/constansts/default-constraints.ts
@@ -1,5 +1,7 @@
 export const DEFAULT_CONSTRAINTS = {
 	usuarios_email_unique: "email",
+	cursos_curso_unique: "curso",
+	serires_serie_unique: "serie",
 };
 
 export type TDeFaultConstraints = keyof typeof DEFAULT_CONSTRAINTS;
