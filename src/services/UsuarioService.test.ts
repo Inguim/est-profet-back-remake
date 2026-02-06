a3a1502dccd3737b5bea8873954f2d4b71ab9fa9
Inguim <igorazevedost@gmail.com>
2026-02-06 14:32:49 -0300

    feat: adiciona criação de relacionamento com Aluno em create de UsuarioService

--- src/services/UsuarioService.test.ts
+++ src/services/UsuarioService.test.ts
@@ -1,53 +1,66 @@
 import { describe, expect, it } from "vitest";
-import { type TFindOneDTO, UsuarioService } from "./UsuarioService.js";
+import {
+	type TCreateDTO,
+	type TCreateDTOAluno,
+	type TCreateDTOProfessor,
+	type TFindOneDTO,
+	UsuarioService,
+} from "./UsuarioService.js";
 import { faker as f } from "@faker-js/faker";
-import { USUARIO_TIPOS, UsuarioDTO } from "../dto/index.js";
+import { USUARIO_TIPOS, UsuarioDTO, type TUsuarioTipo } from "../dto/index.js";
 import { v4 as uuidV4 } from "uuid";
 import { NotFoundError } from "../errors/index.js";
+import { AlunoService, CursoService, SerieService } from "./index.js";
 
-describe("UsuarioService", () => {
-	const service = new UsuarioService();
+describe("UsuarioService", async () => {
+	let uniqueTracker = 0;
+	const service = new UsuarioService({ alunoService: new AlunoService() });
 	const TIPOS = [...USUARIO_TIPOS];
+
 	const WHERE_PARAMS = ["email"];
 
-	it("deve criar um usuário", async () => {
-		const output = await service.create({
+	const cursoModel = new CursoService();
+	const serieModel = new SerieService();
+
+	const curso_id = (await cursoModel.list()).at(0)?.id as string;
+	const serie_id = (await serieModel.list()).at(0)?.id as string;
+
+	const TIPOS_INPUTS = {
+		aluno: { tipo: "aluno", curso_id, serie_id } as TCreateDTOAluno,
+		professor: { tipo: "professor" } as TCreateDTOProfessor,
+	};
+
+	const generateUsuario = (tipo: TUsuarioTipo = "aluno"): TCreateDTO => {
+		uniqueTracker++;
+		return {
 			nome: f.person.firstName(),
-			email: f.internet.email(),
+			email: `email${uniqueTracker}@mail.com`,
 			password: f.internet.password(),
-		});
+			...TIPOS_INPUTS[tipo],
+		};
+	};
+
+	it("deve criar um usuário", async () => {
+		const input = generateUsuario();
+		const output = await service.create(input);
 		expect(output).toBeInstanceOf(UsuarioDTO);
 		expect(output.id).not.toBeNull();
 	});
 
 	it.each(TIPOS)("deve criar um usuário com o tipo: %s", async (tipo) => {
-		const output = await service.create({
-			nome: f.person.firstName(),
-			email: f.internet.email(),
-			password: f.internet.password(),
-			tipo: tipo,
-		});
+		const input = generateUsuario(tipo);
+		const output = await service.create(input);
 		expect(output.tipo).toBe(tipo);
 	});
 
 	it("deve encontrar um usuário pelo ID", async () => {
-		const input = await service.create({
-			nome: f.person.firstName(),
-			email: f.internet.email(),
-			password: f.internet.password(),
-		});
-
+		const input = await service.create(generateUsuario());
 		const output = await service.get(input.id as string);
 		expect(output?.id).toEqual(output?.id);
 	});
 
 	it("deve atualizar as informações do usuário", async () => {
-		const input = await service.create({
-			nome: f.person.firstName(),
-			email: f.internet.email(),
-			password: f.internet.password(),
-			tipo: "aluno",
-		});
+		const input = await service.create(generateUsuario());
 		const output = await service.update(input.id as string, {
 			nome: "nome alterado",
 			email: "email@alterado.com",
@@ -59,11 +72,6 @@ describe("UsuarioService", () => {
 	});
 
 	it("deve não atualizar usuário inexistente", async () => {
-		await service.create({
-			nome: f.person.firstName(),
-			email: f.internet.email(),
-			password: f.internet.password(),
-		});
 		const fakeID = uuidV4();
 		await expect(service.update(fakeID, { nome: "", email: "", tipo: "professor" })).rejects.toBeInstanceOf(
 			NotFoundError,
@@ -71,11 +79,7 @@ describe("UsuarioService", () => {
 	});
 
 	it("deve deletar usuário", async () => {
-		const input = await service.create({
-			nome: f.person.firstName(),
-			email: f.internet.email(),
-			password: f.internet.password(),
-		});
+		const input = await service.create(generateUsuario());
 		const output = await service.delete(input.id as string);
 		expect(output).toBeTruthy();
 	});
@@ -87,10 +91,8 @@ describe("UsuarioService", () => {
 				email: `email${index}@mail.com`,
 				password: `password${index}`,
 			};
-			await service.create({
-				nome: f.person.firstName(),
-				...input,
-			});
+			const usuario = generateUsuario();
+			await service.create({ ...usuario, ...input });
 			const filter = { [String(campo)]: input[campo as keyof TFindOneDTO] };
 			const output = await service.findOne(filter);
 			expect(output.id).not.toBeNull();
--- src/services/UsuarioService.ts
+++ src/services/UsuarioService.ts
@@ -1,10 +1,20 @@
-import { UsuarioDTO } from "../dto/index.js";
+import { UsuarioDTO, type IAlunoDTO } from "../dto/index.js";
 import { UsuarioModel } from "../models/index.js";
+import type { IAlunoService } from "./AlunoService.js";
 import { BaseService, type IBaseService } from "./BaseService.js";
 
-type TCreateDTO = Pick<UsuarioDTO, "nome" | "email" | "tipo" | "password">;
+type TCreateDTOBase = Pick<UsuarioDTO, "nome" | "email" | "tipo" | "password">;
+export type TCreateDTOAluno = { tipo: "aluno" } & Pick<IAlunoDTO, "curso_id" | "serie_id">;
+export type TCreateDTOProfessor = { tipo: "professor" }; // ajustar para ProfessorDTO
+
+export type TCreateDTO = TCreateDTOBase & (TCreateDTOAluno | TCreateDTOProfessor);
 type TUpdateDTO = Partial<Pick<UsuarioDTO, "nome" | "email" | "tipo">>;
 export type TFindOneDTO = Partial<Pick<UsuarioDTO, "email">>;
+
+type TContructorService = {
+	alunoService: IAlunoService;
+	// professorService: IProfessorService;
+};
 
 export interface IUsuarioService extends IBaseService {
 	create(fields: TCreateDTO): Promise<UsuarioDTO>;
@@ -20,4 +30,27 @@ export class UsuarioService
 {
 	protected model = UsuarioModel;
 	protected dto = UsuarioDTO;
+	private alunoService: IAlunoService;
+	// protected professorService: IProfessorService;
+
+	constructor({ alunoService }: TContructorService) {
+		super();
+		this.alunoService = alunoService;
+		// this.professorService = professorService;
+	}
+
+	async create(dto: TCreateDTO): Promise<UsuarioDTO> {
+		const { nome, email, password, tipo } = dto;
+		const model = new this.model();
+		const usuario = await model.create({ nome, email, password, tipo });
+		if (tipo === "aluno") {
+			const { curso_id, serie_id } = dto;
+			await this.alunoService.create({ user_id: usuario.id as string, curso_id, serie_id });
+		}
+		// else {
+		// 	const {} = dto;
+		// 	await this.alunoService.create({ user_id: usuario.id as string });
+		// }
+		return usuario;
+	}
 }
