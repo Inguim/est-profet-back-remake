d6f36f111fcdefdaaaa9e111fc995ec4cf176a98
Inguim <igorazevedost@gmail.com>
2026-02-24 11:35:29 -0300

    fix: corrige dependências faltantes ao instânciar UsuarioService em authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,33 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/index.js";
 import { AuthService } from "../services/AuthService.js";
-import { AlunoService, PasswordService, TokenService, UsuarioService } from "../services/index.js";
+import {
+	AlunoService,
+	CategoriaService,
+	CursoService,
+	PasswordService,
+	ProfessorCategoriaService,
+	ProfessorService,
+	SerieService,
+	TokenService,
+	UsuarioService,
+} from "../services/index.js";
 
 const passwordService = new PasswordService();
 const tokenService = new TokenService();
 const alunoService = new AlunoService();
-const usuarioService = new UsuarioService({ alunoService });
+const categoriaService = new CategoriaService();
+const professorService = new ProfessorService();
+const professorCategoriaService = new ProfessorCategoriaService({ categoriaService });
+const cursoService = new CursoService();
+const serieService = new SerieService();
+const usuarioService = new UsuarioService({
+	alunoService,
+	professorService,
+	professorCategoriaService,
+	cursoService,
+	serieService,
+});
 
 const authService = new AuthService({ passwordService, tokenService, usuarioService });
 const authController = new AuthController(authService);
