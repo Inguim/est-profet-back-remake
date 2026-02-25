56fb422c65bbe99a3d7daef206d146f1ee7de4ac
Inguim <igorazevedost@gmail.com>
2026-02-25 13:59:18 -0300

    feat: adiciona constraint nome de projetos a variavel de mappper para exceções

--- src/utils/constansts/default-constraints.ts
+++ src/utils/constansts/default-constraints.ts
@@ -2,6 +2,7 @@ export const DEFAULT_CONSTRAINTS = {
 	usuarios_email_unique: "email",
 	cursos_curso_unique: "curso",
 	serires_serie_unique: "serie",
+	projetos_nome_unique: "nome",
 };
 
 export type TDeFaultConstraints = keyof typeof DEFAULT_CONSTRAINTS;
