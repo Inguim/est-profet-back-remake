3462b4db1b00300eea1dd1d8f690067fc67c17a1
Inguim <igorazevedost@gmail.com>
2026-02-28 18:54:16 -0300

    feat: adiciona filtros ao metodo list de EstadoService

--- src/services/EstadoService.ts
+++ src/services/EstadoService.ts
@@ -1,17 +1,19 @@
 import { EstadoDTO, type IEstadoDTO } from "../dto/EstadoDTO.js";
-import { EstadoModel } from "../models/EstadoModel.js";
+import { EstadoModel, type TListWhereEstado } from "../models/EstadoModel.js";
+
+export type TListFilterByEstado = TListWhereEstado;
 
 export interface IEstadoService {
-	list(): Promise<IEstadoDTO[]>;
+	list(filters?: TListFilterByEstado): Promise<IEstadoDTO[]>;
 }
 
 export class EstadoService implements IEstadoService {
 	protected model = EstadoModel;
 	protected dto = EstadoDTO;
 
-	async list(): Promise<EstadoDTO[]> {
+	async list(filters?: TListFilterByEstado): Promise<EstadoDTO[]> {
 		const model = new this.model();
-		const estados = await model.list();
+		const estados = await model.list(filters);
 		return estados;
 	}
 }
--- tests/services/EstadoService.test.ts
+++ tests/services/EstadoService.test.ts
@@ -1,13 +1,25 @@
-import { describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import { EstadoDTO } from "../../src/dto/index.js";
 import { EstadoService } from "../../src/services/index.js";
 
 describe("EstadoService", () => {
+	let estado: EstadoDTO;
 	const service = new EstadoService();
+
+	beforeAll(async () => {
+		estado = (await service.list()).at(0) as EstadoDTO;
+	});
 
 	it("deve retornar uma lista de estados", async () => {
 		const output = await service.list();
 		expect(output).toBeInstanceOf(Array);
 		expect(output.at(0)).toBeInstanceOf(EstadoDTO);
+	});
+
+	it("deve permitir filtrar por ID", async () => {
+		const input = String(estado?.id);
+		const output = await service.list();
+		const outputFiltred = await service.list({ id: [input] });
+		expect(output.length).not.toEqual(outputFiltred.length);
 	});
 });
