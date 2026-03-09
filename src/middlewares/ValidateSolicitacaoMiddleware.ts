40b8ce4d660ee5cf6b6218d2767e4feb25f217b5
Inguim <igorazevedost@gmail.com>
2026-03-09 20:08:15 -0300

    feat: adiciona middleware ValidateSolicitacaoMiddleware

---
+++ src/middlewares/ValidateSolicitacaoMiddleware.ts
@@ -0,0 +1,16 @@
+import type { Response, NextFunction } from "express";
+import { solicitacaoValidator } from "../validators/index.js";
+import { ValidationError } from "../errors/ValidationError.js";
+import type { ZodError } from "zod";
+import type { TRequestCreateSolicitacao } from "../controllers/SolicitacaoController.js";
+
+function create(req: TRequestCreateSolicitacao, res: Response, next: NextFunction) {
+	const validator = new solicitacaoValidator.CreateValidator();
+	const result = validator.validate(req.body);
+	if (!result.success) throw new ValidationError(result.extra?.error as ZodError);
+	next();
+}
+
+export const ValidateSolicitacaoMiddleware = {
+	create,
+};
