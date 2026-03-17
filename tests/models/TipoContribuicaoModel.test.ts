63c0b1ebc4a661aa2541d280ccdcb097133c6629
Inguim <igorazevedost@gmail.com>
2026-03-17 20:37:24 -0300

    test: adiciona teste para TipoContribuicaoModel

---
+++ tests/models/TipoContribuicaoModel.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+import { TipoContribuicaoModel } from "../../src/models/index.js";
+import { TipoContribuicaoDTO } from "../../src/dto/index.js";
+
+describe("TipoContribuicaoModel", () => {
+	it("deve encontrar um tipo de contribuição pelo NOME", async () => {
+		const model = new TipoContribuicaoModel();
+		const output = await model.get("apoio_manutencao");
+		expect(output).toBeInstanceOf(TipoContribuicaoDTO);
+		expect(output?.id).toBeDefined();
+	});
+});
