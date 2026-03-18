603d759cdb92e9935b79843e485a4252e6a99e0d
Inguim <igorazevedost@gmail.com>
2026-03-18 15:37:09 -0300

    feat: adiciona validator ContribuidorValidator

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -2,4 +2,5 @@ export * from "./BaseValidator.js";
 export * from "./UsuarioValidator.js";
 export * from "./ProjetoValidator.js";
 export * from "./SolicitacaoValidator.js";
+export * from "./ContribuidorValidator.js";
 export * from "./default-messages.js";
