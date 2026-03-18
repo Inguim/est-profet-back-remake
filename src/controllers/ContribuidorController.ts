23e17929ca7a7114f4b0a08b3c7f3753f661f7f0
Inguim <igorazevedost@gmail.com>
2026-03-18 14:25:38 -0300

    feat: adiciona controller ContribuidorController

---
+++ src/controllers/ContribuidorController.ts
@@ -0,0 +1,51 @@
+import type { NextFunction, Response } from "express";
+import type { IAuthRequest } from "../middlewares/EnsureAuthMiddleware.js";
+import type { IContribuidorService } from "../services/ContribuidorService.js";
+import { STATUS_CODE } from "../utils/constants/status-code.js";
+
+export type TRequestCreateContribuidorDTO = {
+	github_username: string;
+	tipo_contribuicao_id: string;
+	user_id: string;
+};
+
+export type TRequestCreateContribuidor = IAuthRequest<any, any, TRequestCreateContribuidorDTO>;
+
+type TControllerConstructor = {
+	contribuidorService: IContribuidorService;
+};
+
+export class ContribuidorController {
+	private contribuidorService: IContribuidorService;
+
+	constructor({ contribuidorService }: TControllerConstructor) {
+		this.contribuidorService = contribuidorService;
+	}
+
+	async create(req: TRequestCreateContribuidor, res: Response, next: NextFunction): Promise<void> {
+		try {
+			const { github_username, tipo_contribuicao_id, user_id } = req.body;
+			const {
+				id,
+				created_at,
+				updated_at,
+				github_username: github_name,
+				tipo_contribuicao,
+				usuario,
+			} = await this.contribuidorService.create({ github_username, tipo_contribuicao_id, user_id });
+			res.status(STATUS_CODE.CREATED).json({
+				message: "Contribuidor criado com sucesso",
+				data: {
+					id,
+					created_at,
+					updated_at,
+					tipo_contribuicao,
+					usuario,
+					github_username: github_name,
+				},
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
+}
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -7,3 +7,4 @@ export * from "./EstadoController.js";
 export * from "./ProjetoController.js";
 export * from "./SolicitacaoController.js";
 export * from "./NotificacaoController.js";
+export * from "./ContribuidorController.js";
