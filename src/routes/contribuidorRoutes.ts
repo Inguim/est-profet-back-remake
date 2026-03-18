ceca9dabe40e9a3d7056e3d17fb26c0f4c8e5512
Inguim <igorazevedost@gmail.com>
2026-03-18 15:52:36 -0300

    feat: adiciona middleware de validação de body a rota POST /contribuidores

--- src/routes/contribuidorRoutes.ts
+++ src/routes/contribuidorRoutes.ts
@@ -4,15 +4,18 @@ import { ContribuidorService } from "../services/ContribuidorService.js";
 import { GitHubProvider } from "../providers/GitHubProvider.js";
 import { ensureAuthMiddleware } from "../middlewares/EnsureAuthMiddleware.js";
 import { ensureAdminMiddleware } from "../middlewares/EnsureAdminMiddleware.js";
+import { ValidateContribuidorMiddleware } from "../middlewares/ValidateContribuidorMiddleware.js";
 
 const gitHubProvider = new GitHubProvider();
 const contribuidorService = new ContribuidorService({ gitHubProvider });
 
 const contribuidorController = new ContribuidorController({ contribuidorService });
 
+const { create: createMiddleware } = ValidateContribuidorMiddleware;
+
 const router = Router();
 
-router.post("/", ensureAuthMiddleware, ensureAdminMiddleware, (req, res, next) =>
+router.post("/", ensureAuthMiddleware, ensureAdminMiddleware, createMiddleware, (req, res, next) =>
 	contribuidorController.create(req, res, next),
 );
 
