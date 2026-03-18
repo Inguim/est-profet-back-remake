46dba16dae1a90552f3a682cca9aa34defd8e680
Inguim <igorazevedost@gmail.com>
2026-03-18 10:10:17 -0300

    feat: adiciona service TipoContribuicaoService

---
+++ src/services/TipoContribuicaoModel.ts
@@ -0,0 +1,17 @@
+import type { ITipoContribuicaoDTO, TipoContribuicaoDTO } from "../dto/TipoContribuicaoDTO.js";
+import { TipoContribuicaoModel } from "../models/TipoContribuicaoModel.js";
+import type { TTiposContribuicao } from "../utils/constants/tipos-contribuicao.js";
+
+export interface ITipoContribuicaoService {
+	get(nome: TTiposContribuicao): Promise<ITipoContribuicaoDTO>;
+}
+
+export class TipoContribuicaoService implements ITipoContribuicaoService {
+	private model = TipoContribuicaoModel;
+
+	async get(nome: TTiposContribuicao): Promise<TipoContribuicaoDTO> {
+		const model = new this.model();
+		const tipoNotificacao = await model.get(nome);
+		return tipoNotificacao;
+	}
+}
