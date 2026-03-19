1b6e937a768cd2a5ddde9fbabb14b68337726b3a
Inguim <igorazevedost@gmail.com>
2026-03-19 10:55:15 -0300

    feat: adiciona campo user_id ao dto NoticiaDTO

--- src/dto/NoticiaDTO.ts
+++ src/dto/NoticiaDTO.ts
@@ -3,6 +3,7 @@ import type { IBaseDTO } from "./BaseDTO.js";
 export interface INoticiaDTO extends IBaseDTO {
 	nome: string;
 	link: string;
+	user_id: string;
 }
 
 export class NoticiaDTO implements INoticiaDTO {
@@ -11,11 +12,13 @@ export class NoticiaDTO implements INoticiaDTO {
 	readonly updated_at?: Date;
 	readonly nome: string;
 	readonly link: string;
+	readonly user_id: string;
 
-	constructor({ id = null, created_at, updated_at, nome, link }: INoticiaDTO) {
+	constructor({ id = null, created_at, updated_at, nome, link, user_id }: INoticiaDTO) {
 		this.id = id;
 		this.nome = nome;
 		this.link = link;
+		this.user_id = user_id;
 		if (created_at) this.created_at = created_at;
 		if (updated_at) this.updated_at = updated_at;
 	}
--- tests/dto/NoticiaDTO.test.ts
+++ tests/dto/NoticiaDTO.test.ts
@@ -3,7 +3,7 @@ import { NoticiaDTO, type INoticiaDTO } from "../../src/dto/index.js";
 
 describe("NoticiaDTO", () => {
 	it("deve conter as propriedades de INoticiaDTO", () => {
-		const output = new NoticiaDTO({ nome: "teste", link: "asd" });
+		const output = new NoticiaDTO({ nome: "teste", link: "asd", user_id: "asd" });
 		expect(output).toHaveProperty("id");
 		expect(output).toHaveProperty("nome");
 		expect(output).toHaveProperty("link");
@@ -12,7 +12,7 @@ describe("NoticiaDTO", () => {
 	});
 
 	it("deve preencher automaticamente os campos não parciais", () => {
-		const output = new NoticiaDTO({ nome: "teste", link: "asd" });
+		const output = new NoticiaDTO({ nome: "teste", link: "asd", user_id: "asd" });
 		expect(output.id).toBeNull();
 		expect(output.created_at).toBeUndefined();
 		expect(output.updated_at).toBeUndefined();
@@ -23,6 +23,7 @@ describe("NoticiaDTO", () => {
 			id: "id123",
 			nome: "teste",
 			link: "teste",
+			user_id: "asd",
 			created_at: new Date(),
 			updated_at: new Date(),
 		};
