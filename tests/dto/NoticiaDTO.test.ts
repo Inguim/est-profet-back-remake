44bdc93c6e10bbcc425b06428b7949444225fe47
Inguim <igorazevedost@gmail.com>
2026-03-19 10:42:11 -0300

    test: adiciona teste para NoticiaDTO

---
+++ tests/dto/NoticiaDTO.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, it } from "vitest";
+import { NoticiaDTO, type INoticiaDTO } from "../../src/dto/index.js";
+
+describe("NoticiaDTO", () => {
+	it("deve conter as propriedades de INoticiaDTO", () => {
+		const output = new NoticiaDTO({ nome: "teste", link: "asd" });
+		expect(output).toHaveProperty("id");
+		expect(output).toHaveProperty("nome");
+		expect(output).toHaveProperty("link");
+		expect(output).toHaveProperty("created_at");
+		expect(output).toHaveProperty("updated_at");
+	});
+
+	it("deve preencher automaticamente os campos não parciais", () => {
+		const output = new NoticiaDTO({ nome: "teste", link: "asd" });
+		expect(output.id).toBeNull();
+		expect(output.created_at).toBeUndefined();
+		expect(output.updated_at).toBeUndefined();
+	});
+
+	it("deve preencher corretamente os campos", () => {
+		const input: INoticiaDTO = {
+			id: "id123",
+			nome: "teste",
+			link: "teste",
+			created_at: new Date(),
+			updated_at: new Date(),
+		};
+		const output = new NoticiaDTO(input);
+		expect(output).toEqual(input);
+	});
+});
