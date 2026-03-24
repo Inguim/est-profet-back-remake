8d6453e054b05e1b34ff91b01b6cd85bf6a948bd
Inguim <igorazevedost@gmail.com>
2026-03-24 10:33:31 -0300

    fix: corrige utilização de env ao executar testes

--- setupTests.ts
+++ setupTests.ts
@@ -2,7 +2,7 @@ import knex from "knex";
 import config from "./src/database/knexfile.js";
 
 export default async () => {
-	const db = knex(config.development);
+	const db = knex(config.test);
 
 	await db.migrate.rollback(undefined, true);
 	await db.migrate.latest();
